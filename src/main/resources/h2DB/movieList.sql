ea14f56b0215414fc1a8071a91ec53fb34d67f0d
Johny Thomas Kariath <johny.thomas@trv.envestnet.com>
2020-10-27 16:42:16 +0530

    PipelineScript

--- src/main/resources/h2DB/movieList.sql
+++ src/main/resources/h2DB/movieList.sql
@@ -1,1 +1,8 @@
-delete from movie where name='Good Will Hunting'
+delete from movie where name='Good Will Hunting';
+set mode MYSQL;
+insert ignore into movie values (1,0,0,'A Clockwork Orange');
+insert ignore into movie values (2,0,0,'Rangi Taranga');
+insert ignore into movie values (3,0,0,'Masaan');
+insert ignore into movie values (4,0,0,'Afro Samurai');
+insert ignore into movie values (5,0,0,'City of God');
+insert ignore into movie values (6,0,0,'True Grit');
